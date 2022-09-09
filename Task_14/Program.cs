b56ba526c027a59f87aa0057a8665259b177d157
Olesya <lesusha1@yandex.ru>
2022-09-09 16:29:23 +0300

    add Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,22 @@
+﻿// 14. Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 → нет 
+// 46 → нет 
+// 161 → да
+
+
+
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int result1 = number % 7;
+int result2 = number % 23;
+if (result1 == 0 && result2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
